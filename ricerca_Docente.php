b32ea06b9c5454e5b56fcff9fa7fa3300f606f90
Aktobohor <manuel32@hotmail.it>
2017-03-15 12:27:46 +0100

    cambiamenti listadocenti
    aggiunto il controllo sul pagamento del professore nella lista e
    modificato le immagini d default in base al sesso.

--- ricerca_Docente.php
+++ ricerca_Docente.php
@@ -7,6 +7,17 @@
  */
 include ('php_scripts/DB_connection.php');
 include 'header.php';
+
+function dateDifference($date_1 , $date_2 , $differenceFormat = '%a' )
+{
+    $datetime1 = date_create($date_1);
+    $datetime2 = date_create($date_2);
+
+    $interval = $datetime1->diff($datetime2);
+
+    return $interval->format('%R%a');
+
+}
 ?>
 
 <body style="background-image: url('img/head.jpg');  background-attachment: fixed; background-repeat: no-repeat; height: 100%">
@@ -16,10 +27,23 @@ include 'header.php';
         if(isset($_SESSION['isValid']) && $_SESSION['isValid']) {
             $loggato = $conn->query("SELECT * FROM utenti,docenti WHERE utenti.email=docenti.email ORDER BY data_iscrizione DESC ");
             while ($docenti = mysqli_fetch_array($loggato)) {
-                $nome = $docenti["nome"];
-                $cognome = $docenti["cognome"];
-                echo " </div><div><img href=\"#page-top\" src=\"img/logo.png\" width=\"300\" height=\"300\"> </img> </div>";
-                echo "<div style='-webkit-text-fill-color: white;'>" . $nome . " " . $cognome . "</div>";
+
+                $date = date('Y-m-d');
+                $data_iscrizione= $docenti['pagamento_iscrizione'];
+                $giorni_attività = dateDifference($data_iscrizione,$date,'%a');
+                //controllo validità iscrizione
+                if($giorni_attività < 365){
+                    $email = $docenti['email'];
+                    $nome = $docenti["nome"];
+                    $cognome = $docenti["cognome"];
+                    $sesso = $docenti['sesso'];
+                    if ($sesso == "M") {
+                        echo " </div><div><img href=\"#page-top\" src=\"img/default_doc.png\" width=\"200\" height=\"200\"> </img> </div>";
+                    } else {
+                        echo " </div><div><img href=\"#page-top\" src=\"img/default_docF.png\" width=\"200\" height=\"200\"> </img> </div>";
+                    }
+                    echo "<div style='-webkit-text-fill-color: white;'>" . $email . " " . $nome . " " . $cognome . "</div>";
+                }
             }
         }
         else
